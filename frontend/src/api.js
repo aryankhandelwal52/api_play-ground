637d3f89acb00365add15a83a1195953a0f3565f
Aryan Khandelwal <aryankhandelwal467@gmail.com>
2025-09-18 18:45:53 +0530

    use VITE_API_BASE for api base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const base = import.meta.env.VITE_API_BASE || "http://localhost:5000/profile";
+
 const api = axios.create({
-  baseURL: "http://localhost:5000/profile"
+  baseURL: base
 });
 
 export default api;
